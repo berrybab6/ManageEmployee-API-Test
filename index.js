6beeb053f949658afb99d1d6845072e25cb73335
berrybab6 <bedatuassefa@gmail.com>
2022-07-22 21:29:18 +0300

    remove client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+require('dotenv').config()
 
 // const url = "mongodb://localhost:27017/EmployeeDB"
 
