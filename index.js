c74ce82ae4ad8f9dd94fbebba4f674d26e827c20
berrybab6 <bedatuassefa@gmail.com>
2022-07-29 01:56:36 +0300

    modify employee.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var corsOptions = {
     optionsSuccessStatus: 200   // For legacy browser support
 }
 
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin','*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
