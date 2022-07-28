28621c68f6ad6bc17cb7064f0a2ee9799c60956f
berrybab6 <bedatuassefa@gmail.com>
2022-07-29 01:55:09 +0300

    modify employee.js

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ var corsOptions = {
 }
 
 app.use(cors(corsOptions))
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+    next();
+  });
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/EmployeeDB`);
 
