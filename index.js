ecf2e1fd2488a6761879f8f6ac9bea85bf1cc195
berrybab6 <bedatuassefa@gmail.com>
2022-07-29 01:45:03 +0300

    modify employee.js

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ require('dotenv').config()
 const app = express();
 var corsOptions = {
     origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    methods: "GET,OPTIONS,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
-    optionsSuccessStatus: 204   // For legacy browser support
+    optionsSuccessStatus: 200   // For legacy browser support
 }
 
 app.use(cors(corsOptions))
