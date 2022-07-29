6a2f80b3f52b55b7f9a38e2a7bf857db40075d44
berrybab6 <bedatuassefa@gmail.com>
2022-07-29 03:23:14 +0300

    modify employee.js

--- index.js
+++ index.js
@@ -50,6 +50,11 @@ const PORT = process.env.PORT || 9000;
 // })
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const empRouter = require('./src/routers/employee')
 app.use('',empRouter)
 if (process.env.NODE_ENV === 'production') {
